ef987c7f89bb5d85582822851bd9d71a40464000
yunassnn <kasspo20@gmail.com>
2021-07-21 12:56:34 +0900

    bootstrap test project create

---
+++ Bootstrap01/src/main/java/com/work/Bootstrap01Application.java
@@ -0,0 +1,13 @@
+package com.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstrap01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootstrap01Application.class, args);
+	}
+
+}
